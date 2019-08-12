81277e82ac808b38c24b7a83495ec5070faa1373
ElizWyatt <53824368+ElizWyatt@users.noreply.github.com>
2019-08-12 11:06:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var FirstName = prompt("What is your First Name");
+var LastName = prompt("What is your Last Name");
+var age = prompt("What is your age");
+console.log("My Name is " + FirstName + " " + LastName);
+console.log("My Age is " + age);
